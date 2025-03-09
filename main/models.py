00793d094b269e45d480748eced0e1f976584b37
betin <betinvoker1797@gmail.com>
2025-03-10 00:16:54 +0300

    My first commit

---
+++ main/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class VideoPost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField("Название видео", max_length=100)
+    desc = models.TextField("ОПодробное описание к видео")
+    video_file = models.FileField("Загружаемый видео-файл формата MP4", upload_to='videos/')
+    thumbnail = models.ImageField("Титульная картинка формата JPG", upload_to='videos/thumbnail/', default='none')
+    category = models.CharField("Категория видео", max_length=50, default='none')
+    pub_date = models.DateField("Дата публикации", auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    dislikes = models.ManyToManyField(User, related_name='dislikes', blank=True)
+    video_views = models.ManyToManyField(User, related_name='video_views', blank=True)
+
+    def __str__(self):
+        return self.title
+
+class UserData(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    about = models.TextField()
+    profile_pic = models.ImageField(upload_to='pic/', default='pic/default.jpg')
+    subscribers = models.ManyToManyField(User, related_name='subscribers')
+
+class Comment(models.Model):
+    post = models.ForeignKey(VideoPost, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.user.username
