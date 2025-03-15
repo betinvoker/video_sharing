d48196bf17ebb7c43578a496cd7ffcd8ceca6195
betin <betinvoker1797@gmail.com>
2025-03-15 04:31:15 +0300

    Fix page video content and novbar

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import VideoPost, Comment, UserData
 
 # Register your models here.
-admin.site.register(VideoPost)
-admin.site.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user", "post", "comment", "date_create")
+
+class VideoPostAdmin(admin.ModelAdmin):
+    list_display = ("user", "title", "desc", "category")
+
 admin.site.register(UserData)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(VideoPost, VideoPostAdmin)
--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from datetime import date
 from django.contrib.auth.models import User
 # Create your models here.
+
 class VideoPost(models.Model):
     LIST_CATEGORIES = [
         ("Science & Technology","Science & Technology"),
@@ -12,7 +14,7 @@ class VideoPost(models.Model):
     
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField("Название видео", max_length=100)
-    desc = models.TextField("ОПодробное описание к видео")
+    desc = models.TextField("Подробное описание к видео")
     video_file = models.FileField("Загружаемый видео-файл формата MP4", upload_to='videos/')
     thumbnail = models.ImageField("Титульная картинка формата JPG", upload_to='videos/thumbnail/', default='none')
     category = models.CharField("Категория видео", max_length=120,  choices=LIST_CATEGORIES, default="Blogs")
@@ -22,7 +24,7 @@ class VideoPost(models.Model):
     video_views = models.ManyToManyField(User, related_name='video_views', blank=True)
 
     def __str__(self):
-        return self.title
+        return f"{self.user} {self.title} {self.desc} {self.category}"
 
 class UserData(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -31,12 +33,13 @@ class UserData(models.Model):
     subscribers = models.ManyToManyField(User, related_name='subscribers')
 
     def __str__(self):
-        return self.user.username
+        return f"{self.user} {self.about} {self.subscribers} {self.profile_pic}"
 
 class Comment(models.Model):
     post = models.ForeignKey(VideoPost, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.CharField(max_length=300)
+    date_create = models.DateField(("Date"), default=date.today)
 
     def __str__(self):
-        return self.user.username
+        return f"{self.user.id} {self.user.username} {self.post} {self.comment} {self.date_create}"
